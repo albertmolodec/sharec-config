a11ebded2c301f0f3c51c48ed3dda0da11f1c48f
Albert Abdulmanov <albert.abdu@gmail.com>
2021-04-19 06:56:11 +0500

    fix type in eslint config

--- configs/.eslintrc.js
+++ configs/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: ['preact', 'prettier'],
-  plugins: ['@typescript-eslint/parser', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     'prefer-template': 'off',
   },
